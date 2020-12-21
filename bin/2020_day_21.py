dcb900af221e270f0f747becf5906c644baab138
osterbek <75850533+osterbek@users.noreply.github.com>
2020-12-21 15:41:04 +0100

    Update 2020_day_21.py

--- bin/2020_day_21.py
+++ bin/2020_day_21.py
@@ -2,9 +2,7 @@ from pathlib import Path
 
 if __name__ == '__main__':
     dataset = [i for i in Path('../input/input_2020_21.txt').read_text().split('\n')]
-    food = []
-    ingredients = []
-    allergens = []
+    food, ingredients, allergens = [], [], []
     for item in dataset:
         ingre = item.split('(')[0][:-1].split(' ')
         aller = item.split('(')[1][9:-1].split(', ')
@@ -42,14 +40,15 @@ if __name__ == '__main__':
             if not (i in [p[0] for p in possible]):
                 answer_1 += 1
     print('Answer part 1 = {:d} '.format(answer_1), answer_1 == 2211)
-    danger = []
+    canon_dang_ingr = []
     for i in range(0, len(allergens)):
-        danger.append([possible[i][0], allergens[i]])
+        canon_dang_ingr.append([possible[i][0], allergens[i]])
     for i in range(0, len(allergens) - 1):
         for j in range(0, len(allergens) - 1):
-            if danger[j][1] > danger[j+1][1]:
-                temp = danger[j]
-                danger[j] = danger[j+1]
-                danger[j+1] = temp
-    answer_2 = str([i[0] for i in danger]).replace(' ','').replace('[','').replace(']','').replace('\'','')
+            if canon_dang_ingr[j][1] > canon_dang_ingr[j+1][1]:
+                temp = canon_dang_ingr[j]
+                canon_dang_ingr[j] = canon_dang_ingr[j+1]
+                canon_dang_ingr[j+1] = temp
+    answer_2 = str([i[0] for i in canon_dang_ingr]).replace(' ', '').replace('[', '').replace(']', '').replace('\'', '')
     print('Answer part 2 = ' + answer_2 + ' ' + str(answer_2 == 'vv,nlxsmb,rnbhjk,bvnkk,ttxvphb,qmkz,trmzkcfg,jpvz'))
+    
