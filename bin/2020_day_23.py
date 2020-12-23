e1e0c15c48f34d29337afb17eed8079d6df1e7c1
osterbek <75850533+osterbek@users.noreply.github.com>
2020-12-23 08:36:45 +0100

    Add files via upload

---
+++ bin/2020_day_23.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+if __name__ == '__main__':
+    cups = [int(i) for i in Path('../input/input_2020_23.txt').read_text()]
+    for r in range(0, 100):
+        current_cup = cups[0]
+        pick_up = cups[1:4].copy()
+        destination = current_cup - 1
+        while destination in pick_up or destination == 0:
+            destination -= 1
+            if destination < min(cups):
+                destination = max(cups)
+        cups.extend(cups)
+        del cups[0:4]
+        cups = (cups.copy()[:cups.index(destination)+1] + pick_up.copy() + cups.copy()[cups.index(destination)+1:])[:9]
+    answer_1 = int(str(cups[cups.index(1) + 1:] + cups[:cups.index(1)]).replace(', ', '')[1:-1])
+    print('Answer part 1 = {:d} '.format(answer_1), answer_1 == 89372645)
+
+
