caf80a522f0cb1582d7768aebfae638c9fc38973
osterbek <75850533+osterbek@users.noreply.github.com>
2020-12-23 15:15:21 +0100

    Update 2020_day_23.py

--- bin/2020_day_23.py
+++ bin/2020_day_23.py
@@ -1,27 +1,47 @@
-def move(cups):
-    length = len(cups)
-    pick_up = cups[1:4].copy()
-    destination = cups[0] - 1
-    while destination in pick_up or destination == 0:
-        destination -= 1
-        if destination < min(cups):
-            destination = max(cups)
-    cups.extend(cups[0:4])
-    del cups[0:4]
-    cups = (cups.copy()[:cups.index(destination) + 1] + pick_up.copy() + cups.copy()[cups.index(destination) + 1:])[:length]
-    return cups
+def initial_setup(dataset):
+    successors = {}
+    for i in range(0, len(dataset) - 1):
+        successors[int(dataset[i])] = int(dataset[i + 1])
+    successors[int(dataset[len(dataset) - 1])] = int(dataset[0])
+    current = int(dataset[0])
+    return [successors, current]
+
+
+def execute_moves(successors, current, count):
+    for r in range(0, count):
+        pick_up_1 = successors.get(current)
+        pick_up_2 = successors.get(pick_up_1)
+        pick_up_3 = successors.get(pick_up_2)
+        destination = current - 1
+        while (destination in [pick_up_1, pick_up_2, pick_up_3]) or (destination <= 0):
+            destination -= 1
+            if destination <= 0:
+                destination = key_count
+        successors[current] = successors.get(pick_up_3)
+        successors[pick_up_3] = successors.get(destination)
+        successors[destination] = pick_up_1
+        current = successors.get(current)
+    return successors
 
 
 if __name__ == '__main__':
-    cups = [int(i) for i in '614752839']
-    for r in range(0, 100):
-        cups = move(cups)
-    answer_1 = int(str(cups[cups.index(1) + 1:] + cups[:cups.index(1)]).replace(', ', '')[1:-1])
-    print('Answer part 1 = {:d} '.format(answer_1), answer_1 == 89372645)
-    cups = [int(i) for i in '614752839']
-    for i in range(10, 1000000 + 1):
-        cups.append(i)
-    for r in range(0, 10000000):
-        cups = move(cups)
-    answer_2 = (cups[cups.index(1) + 1:] + cups[:cups.index(1)])[:2]
-    print('Answer part 2 = {:d} '.format(int(answer_2[0]) * int(answer_2[1])))
+    dataset = '614752839'
+    [successors_part_1, current_part_1] = initial_setup(dataset)
+    key_count = max(successors_part_1.keys())
+    successors_part_1 = execute_moves(successors_part_1, current_part_1, 100)
+    answer_1 = ''
+    ptr = successors_part_1.get(1)
+    for i in range(0, key_count - 1):
+        answer_1 += str(ptr)
+        ptr = successors_part_1.get(ptr)
+    print('Answer part 1 = {:d} '.format(int(answer_1)), int(answer_1) == 89372645)
+    [successors_part_2, current_part_2] = initial_setup(dataset)
+    key_count = max(successors_part_2.keys())
+    for i in range(key_count + 1, 1000000):
+        successors_part_2[i] = i + 1
+    successors_part_2[1000000] = successors_part_2[key_count]
+    successors_part_2[key_count] = key_count + 1
+    key_count = max(successors_part_2.keys())
+    successors_part_2 = execute_moves(successors_part_2, current_part_2, 10000000)
+    answer_2 = successors_part_2.get(1) * successors_part_2.get(successors_part_2.get(1))
+    print('Answer part 2 = {:d} '.format(answer_2), answer_2 == 21273394210)
