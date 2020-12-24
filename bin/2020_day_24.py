017037e908ffaeb826fc0cfa66121722d87e0634
osterbek <75850533+osterbek@users.noreply.github.com>
2020-12-24 17:07:21 +0100

    Update 2020_day_24.py

--- bin/2020_day_24.py
+++ bin/2020_day_24.py
@@ -1,14 +1,9 @@
 from pathlib import Path
 
 
-def neighbours(a):
-    answer = []
-    for direction in range(0, len(orientation[1])):
-        answer.append((a[0] + orientation[1][direction][0], a[1] + orientation[1][direction][1]))
-    return answer
-
-
 if __name__ == '__main__':
+    def neighbours(a):
+        return [(a[0] + orientation[1][di][0], a[1] + orientation[1][di][1]) for di in range(0, len(orientation[1]))]
     dataset = [i for i in Path('../input/input_2020_24.txt').read_text().split('\n')]
     orientation = [['e', 'se', 'sw', 'w', 'nw', 'ne'], [[2, 0], [1, 1], [-1, 1], [-2, 0], [-1, -1], [1, -1]]]
     floor = {}
@@ -22,12 +17,12 @@ if __name__ == '__main__':
             else:
                 way = dataset[tile][char]
                 char += 1
-            position[0] += orientation[1][orientation[0].index(way)][0]
-            position[1] += orientation[1][orientation[0].index(way)][1]
+            for dim in range(0, 2):
+                position[dim] += orientation[1][orientation[0].index(way)][dim]
         if (position[0], position[1]) in floor.keys():
             del floor[(position[0], position[1])]
         else:
-            floor[(position[0], position[1])] = 1
+            floor[(position[0], position[1])] = True
     print('Answer part 1 = {:d} '.format(len(floor.keys())), len(floor.keys()) == 512)
     for day in range(0, 100):
         shadow = floor.copy()
@@ -45,7 +40,7 @@ if __name__ == '__main__':
                         if tile_3 in floor.keys():
                             neighbours_counter += 1
                     if neighbours_counter == 2:
-                        shadow[tile_2] = 1
+                        shadow[tile_2] = True
         floor = shadow.copy()
         print(day, len(floor.keys()))
     print('Answer part 2 = {:d} '.format(len(floor.keys())), len(floor.keys()) == 4120)
