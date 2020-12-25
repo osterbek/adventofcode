fdb017f0efdf4fdccc4e41874579e826ec1d02fc
osterbek <75850533+osterbek@users.noreply.github.com>
2020-12-25 10:18:18 +0100

    Update 2020_day_25.py

--- bin/2020_day_25.py
+++ bin/2020_day_25.py
@@ -1,9 +1,9 @@
-def determine_size(initial_subject_number, key):
-    value, size = 1, 0
-    while value != key:
-        size += 1
-        value = (value * initial_subject_number) % 20201227
-    return size
+def determine_loop_size(initial_subject_number, reference):
+    key, loop_size = 1, 0
+    while key != reference:
+        loop_size += 1
+        key = (key * initial_subject_number) % 20201227
+    return loop_size
 
 
 def create_key(initial_subject_number, loop_size):
@@ -15,6 +15,6 @@ def create_key(initial_subject_number, loop_size):
 
 if __name__ == '__main__':
     keys = [335121, 363891]
-    encryption_key = create_key(create_key(7, determine_size(7, keys[0])), determine_size(7, keys[1]))
+    encryption_key = create_key(create_key(7, determine_loop_size(7, keys[0])), determine_loop_size(7, keys[1]))
     print('Answer part 1 = {:d} '.format(encryption_key), encryption_key == 9420461)
     print('Answer part 2 = {:d} '.format(50), 50 == 50)
